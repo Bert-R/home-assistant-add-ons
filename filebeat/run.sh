8acf49097a606f515605a913cb96a2746292acba
Bert Roos <Bert-R@users.noreply.github.com>
2023-02-14 20:41:59 +0100

    Add debug info

--- filebeat/run.sh
+++ filebeat/run.sh
@@ -4,4 +4,6 @@ export ELASTICSEARCH_HOSTS=$(bashio::config 'elasticsearch_hosts')
 export ELASTICSEARCH_USERNAME=$(bashio::config 'elasticsearch_username')
 export ELASTICSEARCH_PASSWORD=$(bashio::config 'elasticsearch_password')
 
+pwd
+ls -l
 ./filebeat -e -c /config/filebeat.yml --path.data /data
