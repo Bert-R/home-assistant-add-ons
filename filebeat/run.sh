ed0b7ca93628f0baa5a223e136aa85ca9453d19e
Bert Roos <Bert-R@users.noreply.github.com>
2023-02-14 20:53:28 +0100

    Run filebeat from /usr/bin

--- filebeat/run.sh
+++ filebeat/run.sh
@@ -4,6 +4,9 @@ export ELASTICSEARCH_HOSTS=$(bashio::config 'elasticsearch_hosts')
 export ELASTICSEARCH_USERNAME=$(bashio::config 'elasticsearch_username')
 export ELASTICSEARCH_PASSWORD=$(bashio::config 'elasticsearch_password')
 
+set -x
 echo "Source URL: "
 cat src_url
-./filebeat -e -c /config/filebeat.yml --path.data /data
+which filebeat
+echo $PATH
+filebeat -e -c /config/filebeat.yml --path.data /data
